cdedd320ab6744ffe143e68fbc950ae08c959b2c
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-03-16 14:12:22 -0300

    Requisições dinâmicas

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -17,14 +17,17 @@ public class RotaPedidos {
 			public void configure() throws Exception {
 				
 				from("file:pedidos?delay=5s&noop=true").
+					setProperty("pedidoId", xpath("/pedido/id/text()")).
+					setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
 					split().
 						xpath("/pedido/itens/item").
 					filter().
 						xpath("/item/formato[text()='EBOOK']").
+						setProperty("ebookId", xpath("/item/livro/codigo/text()")).
 					marshal().xmljson().
 					log("${id} - ${body}").
 					setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
-					setHeader(Exchange.HTTP_QUERY, constant("ebookId=ARQ&pedidoId=2451256&clienteId=edgar.b@abc.com")).
+					setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
 				to("http4://localhost:8080/webservices/ebook/item");
 			}
 		});
